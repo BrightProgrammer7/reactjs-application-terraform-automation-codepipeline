07d1e2e0a12ad7723932bbfe12365f79c9353911
BrightProgrammer7 <e-akhmim.a@ucd.ma>
2024-01-26 20:16:53 +0000

    terra

--- variables.tf
+++ variables.tf
@@ -7,22 +7,22 @@
 # }
 
 variable "pipeline_bucket_name" {
-  default     = "webapp-abdelilah"
+  default     = "webapp-akhmim"
   description = "pipeline bucket name"
 }
 
 variable "app_bucket_name" {
-  default     = "webapp-bucket-abdelilah"
+  default     = "webapp-bucket-akhmim"
   description = "app bucket name"
 }
 
 variable "projectname" {
-  default     = "TerraformAWS"
+  default     = "TerraformReact"
   description = "pipeline project name"
 }
 
 variable "repo_id" {
-  default     = "BrightProgrammer7/TerraformAWS"
+  default     = "BrightProgrammer7/TerraformReact"
   description = "github repository id"
 }
 
@@ -34,4 +34,5 @@ variable "repo_branch_name" {
 variable "connection_arn" {
   type        = string
   description = "Arn for the CodeStar Connection"
+  default = "arn:aws:codepipeline:us-east-1:010993157985:TerraformReact-pipeline"
 }
